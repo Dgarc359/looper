896aa334b5ee9cdcc4fbc835a1df757574c06dfb
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2024-03-06 23:12:24 -0500

    metronome with example

--- src/stores/metronomeStore.ts
+++ src/stores/metronomeStore.ts
@@ -1,4 +1,8 @@
-import {atom} from "nanostores";
+import {atom, map} from "nanostores";
 
+export const metronome = map({
+  bpm: 80,
+  isMuted: true,
+})
 export const bpm = atom(80);
 export const metronomeIsMuted = atom(true);
