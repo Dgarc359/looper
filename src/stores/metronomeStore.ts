da95254c32f110643033f18952b7f9586dc331c8
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2024-03-08 04:31:11 -0500

    tone is running callback

--- src/stores/metronomeStore.ts
+++ src/stores/metronomeStore.ts
@@ -2,7 +2,7 @@ import {atom, map} from "nanostores";
 
 export const metronome = map({
   bpm: 80,
-  isMuted: true,
+  isPlaying: true,
 })
 export const bpm = atom(80);
 export const metronomeIsMuted = atom(true);
