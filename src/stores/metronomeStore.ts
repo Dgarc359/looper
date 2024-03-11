225a6858ece990095bcd6b4315b3e59b561fe457
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2024-03-10 21:03:07 -0400

    play block sound through tone player and stop according to user interaction

--- src/stores/metronomeStore.ts
+++ src/stores/metronomeStore.ts
@@ -2,7 +2,7 @@ import {atom, map} from "nanostores";
 
 export const metronome = map({
   bpm: 80,
-  isPlaying: true,
+  isPlaying: false,
 })
 export const bpm = atom(80);
 export const metronomeIsMuted = atom(true);
